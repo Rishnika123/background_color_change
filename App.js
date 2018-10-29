6329b0611b37ffda1be576d6b2fe9d0ff91bd4f1
Rishnika Mekala <rishnikareddy1410@gmail.com>
2018-10-30 00:11:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+const colorArray = ['red', 'lightblue', 'pink', 'yellow', 'brown', 'magenta'];
+
+class HelloWorld extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			color: 'ROSYBROWN',
+		};
+	}
+
+	changeColor(e) {
+		this.setState({
+			color: e.target.value,
+		});
+	}
+
+	render() {
+		// console.log(this.state);
+		const stylesObj = {
+			background: this.state.color,
+		};
+
+		return (
+			<div id="hello-world" style={stylesObj} className="container">
+				<div className="box">
+					<h2>Hello, World!</h2>
+
+					<br />
+					<input value={this.state.color} onChange={this.changeColor.bind(this)} />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default HelloWorld;
